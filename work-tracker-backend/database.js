477f3b39bcd30608b04ccf9513a612ca12e76af5
Bartek <bbogucki7@gmail.com>
2025-01-05 08:53:23 +0100

    create database, implement into register.js and change register endpoint as async function

---
+++ work-tracker-backend/database.js
@@ -0,0 +1,25 @@
+const mysql = require("mysql2");
+
+const db = mysql.createConnection({
+	host: "localhost",
+	user: "root",
+	password: "monkeyking",
+	database: "work-tracker",
+});
+
+db.query(`CREATE TABLE IF NOT EXISTS userData (
+    ID int PRIMARY KEY AUTO_INCREMENT,
+    firstName varchar(255),
+	lastName varchar(255),
+	email varchar (255) UNIQUE,
+	password varchar(255)
+)`);
+
+db.connect(error => {
+	if (error) {
+		console.error("Error connect: ", error.stack);
+		return;
+	}
+});
+
+module.exports = db;
--- work-tracker-backend/register.js
+++ work-tracker-backend/register.js
@@ -1,8 +1,28 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
-const cookieParser = require("cookie-parser");
 const router = express.Router();
+const db = require("./database");
 
-router.post("/register", (req, res) => {});
+router.post("/register", async (req, res) => {
+	try {
+		const { firstName, lastName, email, password } = req.body;
+		if (!firstName || !lastName || !email || !password) {
+			return res.status(400).send("All fields are required!");
+		}
+
+		const [existingUser] = await db.promise().query(`SELECT * FROM userData WHERE email = ?`);
+		if (existingUser.length > 0) return res.status(400).send("User with this email is already exists!");
+
+		const hashedPassword = await bcrypt.hash(password, 8);
+		const registerUserToDataBase = `INSERT INTO userData (firstName, lastName, email, password) VALUES (?, ?, ?, ?)`;
+
+		await db.promise().query(registerUserToDataBase, [firstName, lastName, email, hashedPassword]);
+
+		return res.status(201).json({ message: "User registered successfully!" });
+	} catch (error) {
+		console.error("Error during registration:", error);
+		return res.status(500).send("Server error!");
+	}
+});
 
 module.exports = router;
