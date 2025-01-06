3401ace0bfc7457c47da09ccb4eb85873f05c1d0
Bartek <bbogucki7@gmail.com>
2025-01-06 11:21:51 +0100

    set SignUp data fields, userData states and inputs

--- work-tracker-frontend/src/components/sing-components/SignUp.tsx
+++ work-tracker-frontend/src/components/sing-components/SignUp.tsx
@@ -1,8 +1,32 @@
 import { useState } from "react";
 import { Button, Col, Container, Form, Row } from "react-bootstrap";
+import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
+import { AppDispatch } from "../../../store/store";
+import { UserRegister } from "../../../store/userSlice";
+
+interface UserRegisterData {
+	firstName: string;
+	lastName: string;
+	email: string;
+	password: string;
+}
 
 export default function SignUp() {
+	const dispatch = useDispatch<AppDispatch>();
+	const [userData, setUserData] = useState<UserRegisterData>({ firstName: "", lastName: "", email: "", password: "" });
+
+	const handleInputOnChangeData = (key: string, value: string) => {
+		setUserData(prevState => ({
+			...prevState,
+			[key]: value,
+		}));
+	};
+
+	const handleAcceptUserRegister = () => {
+		dispatch(UserRegister());
+	};
+
 	return (
 		<div className='sign-in-up vh-100 d-flex justify-content-center align-items-center'>
 			<Container>
@@ -12,22 +36,33 @@ export default function SignUp() {
 						<Form>
 							<Form.Group>
 								<Form.Label>First Name</Form.Label>
-								<Form.Control />
+								<Form.Control
+									onChange={e => handleInputOnChangeData("firstName", e.target.value)}
+									value={userData.firstName}
+								/>
 							</Form.Group>
 							<Form.Group>
 								<Form.Label>Last Name</Form.Label>
-								<Form.Control />
+								<Form.Control
+									onChange={e => handleInputOnChangeData("lastName", e.target.value)}
+									value={userData.lastName}
+								/>
 							</Form.Group>
 							<Form.Group>
 								<Form.Label>Email</Form.Label>
-								<Form.Control />
+								<Form.Control onChange={e => handleInputOnChangeData("email", e.target.value)} value={userData.email} />
 							</Form.Group>
 							<Form.Group>
 								<Form.Label>Password</Form.Label>
-								<Form.Control />
+								<Form.Control
+									onChange={e => handleInputOnChangeData("password", e.target.value)}
+									value={userData.password}
+								/>
 							</Form.Group>
 						</Form>
-						<Button className='btn w-100 mt-4 mb-4'>Register</Button>
+						<Button className='btn w-100 mt-4 mb-4' onClick={handleAcceptUserRegister}>
+							Register
+						</Button>
 						<Link to='/login'>
 							<Button className='btn w-100 mt-4'>Sign in your account</Button>
 						</Link>
--- work-tracker-frontend/src/main.tsx
+++ work-tracker-frontend/src/main.tsx
@@ -3,9 +3,13 @@ import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../sass/_main.scss";
 import App from "./App.tsx";
+import store from "../store/store.ts";
+import { Provider } from "react-redux";
 
 createRoot(document.getElementById("root")!).render(
-	<StrictMode>
-		<App />
-	</StrictMode>
+	<Provider store={store}>
+		<StrictMode>
+			<App />
+		</StrictMode>
+	</Provider>
 );
