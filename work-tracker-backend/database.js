dc3b8c9d0cd75c1d64b2f874c3f9d99eb2d72975
Bartek <bbogucki7@gmail.com>
2025-01-06 12:25:52 +0100

    change database name

--- work-tracker-backend/database.js
+++ work-tracker-backend/database.js
@@ -4,7 +4,7 @@ const db = mysql.createConnection({
 	host: "localhost",
 	user: "root",
 	password: "monkeyking",
-	database: "work-tracker",
+	database: "work_tracker",
 });
 
 db.query(`CREATE TABLE IF NOT EXISTS userData (
