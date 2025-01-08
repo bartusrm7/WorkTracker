96a4035f0fda79a801cb4e097c58a333ddc948ef
Bartek <bbogucki7@gmail.com>
2025-01-08 15:06:58 +0100

    add setInterval into SignIn handle function and add spinner to delay loading page

--- work-tracker-frontend/src/components/sing-components/SignIn.tsx
+++ work-tracker-frontend/src/components/sing-components/SignIn.tsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import { Button, Col, Container, Form, Row } from "react-bootstrap";
 import { useDispatch } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { AppDispatch } from "../../../store/store";
 import { UserLogin } from "../../../store/authSlice";
+import Spinner from "react-bootstrap/Spinner";
 
 interface UserLoginData {
 	email: string;
@@ -13,6 +14,8 @@ interface UserLoginData {
 export default function SignIn() {
 	const dispatch = useDispatch<AppDispatch>();
 	const [userData, setUserData] = useState<UserLoginData>({ email: "", password: "" });
+	const [isLoadingPage, setIsLoadingPage] = useState<boolean>(false);
+	const navigate = useNavigate();
 
 	const handleInputOnChangeData = (key: string, value: string) => {
 		setUserData(prevState => ({
@@ -23,34 +26,51 @@ export default function SignIn() {
 
 	const handleAcceptUserLogin = () => {
 		dispatch(UserLogin(userData));
+		setIsLoadingPage(!isLoadingPage);
+
+		setInterval(() => {
+			navigate("/dashboard");
+		}, 2000);
 	};
 
 	return (
 		<div className='sign-in-up vh-100 d-flex justify-content-center align-items-center'>
 			<Container>
 				<Row>
-					<Col className='text m-auto' xs={10} md={8} lg={6} xl={5}>
-						<div className='header mb-4 text-center'>LOGIN</div>
-						<Form>
-							<Form.Group>
-								<Form.Label>Email</Form.Label>
-								<Form.Control onChange={e => handleInputOnChangeData("email", e.target.value)} value={userData.email} />
-							</Form.Group>
-							<Form.Group>
-								<Form.Label>Password</Form.Label>
-								<Form.Control
-									onChange={e => handleInputOnChangeData("password", e.target.value)}
-									value={userData.password}
-								/>
-							</Form.Group>
-						</Form>
-						<Button className='btn w-100 mt-4 mb-4' onClick={handleAcceptUserLogin}>
-							Login
-						</Button>
-						<Link to='/register'>
-							<Button className='btn w-100 mt-4'>Create new account</Button>
-						</Link>
-					</Col>
+					{isLoadingPage ? (
+						<Col className='sign-in-up__spinner d-flex justify-content-center align-items-center'>
+							<span className='sign-in-up__loading-span'>Loading...</span>
+							<Spinner animation='border' size='sm' role='status'>
+								<span className='visually-hidden'>Loading...</span>
+							</Spinner>
+						</Col>
+					) : (
+						<Col className='text m-auto' xs={10} md={8} lg={6} xl={5}>
+							<div className='header mb-4 text-center'>LOGIN</div>
+							<Form>
+								<Form.Group>
+									<Form.Label>Email</Form.Label>
+									<Form.Control
+										onChange={e => handleInputOnChangeData("email", e.target.value)}
+										value={userData.email}
+									/>
+								</Form.Group>
+								<Form.Group>
+									<Form.Label>Password</Form.Label>
+									<Form.Control
+										onChange={e => handleInputOnChangeData("password", e.target.value)}
+										value={userData.password}
+									/>
+								</Form.Group>
+							</Form>
+							<Button className='btn w-100 mt-4 mb-4' onClick={handleAcceptUserLogin}>
+								Login
+							</Button>
+							<Link to='/register'>
+								<Button className='btn w-100 mt-4'>Create new account</Button>
+							</Link>
+						</Col>
+					)}
 				</Row>
 			</Container>
 		</div>
