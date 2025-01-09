69581f2b54ed47da0601660133ceaedeb0e9cabd
Bartek <bbogucki7@gmail.com>
2025-01-09 11:46:38 +0100

    stylize Navigation

--- work-tracker-frontend/src/components/navigation-components/Navigation.tsx
+++ work-tracker-frontend/src/components/navigation-components/Navigation.tsx
@@ -18,11 +18,14 @@ export default function Navigation() {
 		<div className='navigation vh-100'>
 			<Hamburger toggled={isMenuOpened} toggle={() => setIsMenuOpened(!isMenuOpened)} />
 
-			<nav className='navigation__main-container d-flex flex-column'>
-				<div className='navigation__nav-container mt-5 text-center d-flex flex-column justify-content-between'>
+			<nav
+				className={`navigation__main-container d-flex flex-column justify-content-between justify-content-lg-center ${
+					isMenuOpened ? "d-flex" : "d-none d-lg-flex"
+				}`}>
+				<div className='navigation__nav-container mt-5 text-center d-flex flex-column justify-content-lg-between'>
 					{navLocation.map((item, index) => (
 						<Link to={item.location} key={index}>
-							<Button className='navigation__nav-btn mb-3 mb-lg-0'>{item.label}</Button>
+							<Button className='navigation__nav-btn mb-3 mt-3 mb-lg-0 mt-lg-0'>{item.label}</Button>
 						</Link>
 					))}
 				</div>
