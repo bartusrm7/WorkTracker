e92d0ff515caab8d52d61da3a5b122d3e82cf5e9
Bartek <bbogucki7@gmail.com>
2025-02-01 11:44:30 +0100

    correct SignUp handleValidationForm function

--- work-tracker-frontend/store/userSlice.ts
+++ work-tracker-frontend/store/userSlice.ts
@@ -36,12 +36,36 @@ export const UserRegister = createAsyncThunk<User, User>(
 				throw new Error(`Error ${response.status}: ${errorText}`);
 			}
 			const data = await response.json();
-			return { firstName: data.firstName, lastName: data.lastName, email: data.email, password: data.password };
+			return data;
 		} catch (error) {
 			return rejectWithValue("Error during registration!");
 		}
 	}
 );
+
+// export const UserRegister = createAsyncThunk<User, User>(
+// 	"user/user-register",
+// 	async (userData, { rejectWithValue }) => {
+// 		try {
+// 			const response = await fetch("http://localhost:5174/register", {
+// 				method: "POST",
+// 				headers: {
+// 					"Content-type": "application/json",
+// 				},
+// 				body: JSON.stringify(userData),
+// 				credentials: "include",
+// 			});
+// 			if (!response.ok) {
+// 				const errorText = await response.text();
+// 				throw new Error(`Error ${response.status}: ${errorText}`);
+// 			}
+// 			const data = await response.json();
+// 			return { firstName: data.firstName, lastName: data.lastName, email: data.email, password: data.password };
+// 		} catch (error) {
+// 			return rejectWithValue("Error during registration!");
+// 		}
+// 	}
+// );
 
 const userSlice = createSlice({
 	name: "user",
@@ -50,8 +74,8 @@ const userSlice = createSlice({
 	extraReducers: builder => {
 		builder
 			.addCase(UserRegister.fulfilled, (state, action: PayloadAction<User>) => {
-				state.loading = false;
 				state.user.push(action.payload);
+				state.loading = false;
 				state.status = "success";
 			})
 			.addCase(UserRegister.rejected, state => {
