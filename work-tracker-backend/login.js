b7ce1e12132ba73a6378595bab46e010c3c8f84d
Bartek <bbogucki7@gmail.com>
2025-02-01 11:08:02 +0100

    correct register and login enpoints in backend

--- work-tracker-backend/login.js
+++ work-tracker-backend/login.js
@@ -2,15 +2,15 @@ require("dotenv").config();
 const express = require("express");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const router = express.Router();
 const db = require("./database");
+const router = express.Router();
 
 function authenticateToken(req, res, next) {
 	const authHeader = req.headers["authorization"];
 	const token = authHeader && authHeader.split(" ")[1];
 
 	if (!token) {
-		return res.status(401).send({ message: "No token provided!" });
+		return res.status(401).json({ message: "Token is not provide!" });
 	}
 
 	jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
@@ -23,8 +23,7 @@ function authenticateToken(req, res, next) {
 }
 
 router.post("/authorization", authenticateToken, (req, res) => {
-	console.log(req.user);
-	res.status(200).json({ message: "Authorization successful!", user: req.user });
+	return res.status(200).json({ message: "", user: req.user });
 });
 
 router.post("/login", async (req, res) => {
@@ -34,39 +33,43 @@ router.post("/login", async (req, res) => {
 			return res.status(400).send("All fields are required!");
 		}
 
-		const [result] = await db.promise().query(`SELECT * FROM userData WHERE email = ?`, [email]);
-		if (result.length === 0) {
-			return res.status(401).send("Invalid email or password!");
-		}
+		const checkUserQuery = `SELECT * FROM userData WHERE email = ?`;
+		db.query(checkUserQuery, [email], (err, data) => {
+			if (err) {
+				return res.status(500).json({ error: "Database query error", details: err });
+			}
+			if (data.length === 0) {
+				return res.status(404).json({ error: "User not found!" });
+			}
 
-		const user = result[0];
+			const user = data[0];
+			const isPasswordValid = bcrypt.compare(password, user.password);
+			if (!isPasswordValid) {
+				return res.status(401).json({ error: "Password is not correct!" });
+			}
 
-		const isMatch = await bcrypt.compare(password, user.password);
-		if (!isMatch) {
-			return res.status(401).send("Invalid email or password!");
-		}
+			const accessToken = jwt.sign({ email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "15m" });
+			const refreshToken = jwt.sign({ email }, process.env.REFRESH_TOKEN_SECRET, { expiresIn: "30d" });
 
-		const accessToken = jwt.sign({ email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "15m" });
-		const refreshToken = jwt.sign({ email }, process.env.REFRESH_TOKEN_SECRET, { expiresIn: "30d" });
+			res.cookie("accessToken", accessToken, {
+				maxAge: 15 * 60 * 1000,
+				secure: process.env.NODE_ENV === "production",
+				httpOnly: true,
+				sameSite: "Lax",
+			});
 
-		res.cookie("accessToken", accessToken, {
-			maxAge: 15 * 60 * 1000,
-			secure: process.env.NODE_ENV === "production",
-			httpOnly: true,
-			sameSite: "Lax",
+			res.cookie("refreshToken", refreshToken, {
+				maxAge: 30 * 24 * 60 * 60 * 1000,
+				secure: process.env.NODE_ENV === "production",
+				httpOnly: true,
+				sameSite: "Lax",
+			});
+
+			res.status(200).json({ message: "User logged successfully!", isLogged: true });
 		});
-
-		res.cookie("refreshToken", refreshToken, {
-			maxAge: 30 * 24 * 60 * 60 * 1000,
-			secure: process.env.NODE_ENV === "production",
-			httpOnly: true,
-			sameSite: "Lax",
-		});
-
-		return res.status(200).json({ message: "User logged successfully!", isLogged: true });
 	} catch (error) {
-		console.error("Error during login:", error);
-		return res.status(500).send("Internal server error!");
+		console.error("Error during user login:", error);
+		return res.status(500).json({ error: "Internal server error!" });
 	}
 });
 
--- work-tracker-backend/register.js
+++ work-tracker-backend/register.js
@@ -1,27 +1,42 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
-const router = express.Router();
 const db = require("./database");
+const router = express.Router();
 
 router.post("/register", async (req, res) => {
 	try {
 		const { firstName, lastName, email, password } = req.body;
+
 		if (!firstName || !lastName || !email || !password) {
-			return res.status(400).send("All fields are required!");
+			return res.status(400).json({ error: "All fields are required" });
 		}
 
-		const [existingUser] = await db.promise().query(`SELECT * FROM userData WHERE email = ?`, [email]);
-		if (existingUser.length > 0) return res.status(400).send("User with this email is already exists!");
+		const checkUserQuery = `SELECT * FROM userData WHERE email = ?`;
+		db.query(checkUserQuery, [email], (err, data) => {
+			if (err) {
+				console.error("Database query error during user check:", err.message);
+				return res.status(500).json({ error: "Internal server error" });
+			}
+			if (data.length > 0) {
+				return res.status(409).json({ error: "User with this email address is already exists!" });
+			}
 
-		const hashedPassword = await bcrypt.hash(password, 8);
-		const registerUserToDataBase = `INSERT INTO userData (firstName, lastName, email, password) VALUES (?, ?, ?, ?)`;
+			const salt = bcrypt.genSaltSync(8);
+			const hashedPassword = bcrypt.hashSync(password, salt);
 
-		await db.promise().query(registerUserToDataBase, [firstName, lastName, email, hashedPassword]);
+			const createUserQuery = `INSERT INTO userData (firstName, lastName, email, password) VALUES (?, ?, ?, ?)`;
+			db.query(createUserQuery, [firstName, lastName, email, hashedPassword], err => {
+				if (err) {
+					console.error("Database query error during user creation:", err.message);
+					return res.status(500).json({ error: "Internal server error" });
+				}
 
-		return res.status(201).json({ message: "User registered successfully!" });
+				return res.status(201).json({ message: "User registered successfully!" });
+			});
+		});
 	} catch (error) {
 		console.error("Error during registration:", error);
-		return res.status(500).send("Server error!");
+		return res.status(500).json({ error: "Internal server error!" });
 	}
 });
 
