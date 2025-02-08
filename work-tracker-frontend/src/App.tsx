baa15545231f71f6114c10f2817112d2b322935a
Bartek <bbogucki7@gmail.com>
2025-02-08 19:34:23 +0100

    implement refreshToken dipatch into App.tsx

--- work-tracker-frontend/src/App.tsx
+++ work-tracker-frontend/src/App.tsx
@@ -5,8 +5,22 @@ import SignIn from "./components/sing-components/SignIn";
 import MainContainer from "./components/dashboard-components/MainContainer";
 import ProtectedRoute from "./components/sing-components/ProtectedRoute";
 import MyTasks from "./components/dashboard-components/MyTasks";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "../store/store";
+import { RefreshAccessTokenAfterExpired } from "../store/authSlice";
+import Cookies from "js-cookie";
 
 export default function App() {
+	const dispatch = useDispatch<AppDispatch>();
+	const refreshToken = Cookies.get("refreshToken");
+
+	useEffect(() => {
+		if (refreshToken) {
+			dispatch(RefreshAccessTokenAfterExpired(refreshToken));
+		}
+	}, [refreshToken]);
+
 	return (
 		<BrowserRouter>
 			<Routes>
