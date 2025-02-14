13fb3c3ada0864c043a5690b0598e617c1c4a8f9
Bartek <bbogucki7@gmail.com>
2025-02-14 15:08:41 +0100

    complete get-task endpoint in tasks backend

--- work-tracker-backend/tasks.js
+++ work-tracker-backend/tasks.js
@@ -5,10 +5,14 @@ const router = express.Router();
 
 router.post("/create-task", async (req, res) => {
 	try {
-		const { accessToken, taskName, taskDate, taskDescription } = req.body;
+		const { taskName, taskDate, taskDescription } = req.body;
+		const accessToken = req.cookies.accessToken;
 
-		if (!taskName || !taskDate || !taskDescription) {
+		if (!taskName || !taskDate) {
 			return res.status(400).json({ error: "All fields are required!" });
+		}
+		if (!accessToken) {
+			return res.status(401).json({ error: "Access token missing!" });
 		}
 
 		jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
@@ -35,7 +39,27 @@ router.post("/create-task", async (req, res) => {
 
 router.get("/get-task", async (req, res) => {
 	try {
-	} catch (error) {}
+		const accessToken = req.cookies.accessToken;
+
+		jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+			if (err) {
+				return res.status(403).json({ error: "Invalid token!" });
+			}
+			const email = decoded.email;
+
+			const getTasksFromDatabase = `SELECT * FROM tasksData WHERE email = ?`;
+			db.query(getTasksFromDatabase, [email], (err, results) => {
+				if (err) {
+					return res.status(500).json({ error: "Database query error", details: err });
+				}
+
+				return res.status(200).json({ message: "Task gotten successfully!" }, { tasks: results });
+			});
+		});
+	} catch (error) {
+		console.error("Error during getting tasks:", error);
+		return res.status(500).json({ error: "Internal server error!" });
+	}
 });
 
 module.exports = router;
