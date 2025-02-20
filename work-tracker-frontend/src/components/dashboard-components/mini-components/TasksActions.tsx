e85e2b4ff6e5fd1ec7a146ae24a67287f16d6031
Bartek <bbogucki7@gmail.com>
2025-02-20 11:27:55 +0100

    change logic to open action buttons in TasksActions

--- work-tracker-frontend/src/components/dashboard-components/mini-components/TasksActions.tsx
+++ work-tracker-frontend/src/components/dashboard-components/mini-components/TasksActions.tsx
@@ -3,30 +3,39 @@ import DoneIcon from "@mui/icons-material/Done";
 import ModeEditIcon from "@mui/icons-material/ModeEdit";
 import DeleteIcon from "@mui/icons-material/Delete";
 import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "../../../../store/store";
+import { RemoveTaskAction } from "../../../../store/tasksActionsSlice";
 
 export default function TasksActions() {
-	const [isTasksOpened, setIsTasksOpened] = useState<boolean>(true);
+	const dispatch = useDispatch<AppDispatch>();
+	const [isTasksOpened, setIsTasksOpened] = useState<boolean>(false);
+
+	const handleRemoveTask = () => {
+		dispatch(RemoveTaskAction());
+	};
 
 	return (
 		<div className='tasks-actions'>
-			<div className='tasks-actions__main-container'>
-				{isTasksOpened ? (
-					<Button className='tasks-actions__replacement-field' onClick={() => setIsTasksOpened(!isTasksOpened)}>
-						...
-					</Button>
-				) : (
+			<div className='tasks-actions__main-container d-flex'>
+				{isTasksOpened && (
 					<div className='d-flex'>
-						<Button>
+						<Button className='tasks-actions__action-btn custom-btn'>
 							<DoneIcon />
 						</Button>
-						<Button>
+						<Button className='tasks-actions__action-btn custom-btn'>
 							<ModeEditIcon />
 						</Button>
-						<Button>
+						<Button className='tasks-actions__action-btn custom-btn' onClick={() => handleRemoveTask()}>
 							<DeleteIcon />
 						</Button>
 					</div>
 				)}
+				<Button
+					className='tasks-actions__replacement-field custom-btn'
+					onClick={() => setIsTasksOpened(!isTasksOpened)}>
+					...
+				</Button>
 			</div>
 		</div>
 	);
