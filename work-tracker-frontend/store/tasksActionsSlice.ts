7552fcb35393ef0dd8b4e0d50ff348983ca29030
Bartek <bbogucki7@gmail.com>
2025-02-28 11:02:34 +0100

    create DoneTaskAction in tasksActionsSlice, implement all logic and extraReducers

--- work-tracker-frontend/store/tasksActionsSlice.ts
+++ work-tracker-frontend/store/tasksActionsSlice.ts
@@ -19,45 +19,29 @@ const initialState: TasksState = {
 	loading: false,
 };
 
-// export const DoneTaskAction = createAsyncThunk<Tasks>("tasksAction/done-task", async (_, { rejectWithValue }) => {
-// 	try {
-// 		const response = await fetch("http://localhost:5174/done-task", {
-// 			method: "POST",
-// 			headers: {
-// 				"Content-type": "application/json",
-// 			},
-// 			credentials: "include",
-// 		});
-// 		if (!response.ok) {
-// 			const errorText = await response.text();
-// 			throw new Error(`Error ${response.status}: ${errorText}`);
-// 		}
-// 		const data = await response.json();
-// 		return data;
-// 	} catch (error) {
-// 		return rejectWithValue("Error during marking task as done!");
-// 	}
-// });
-
-// export const EditTaskAction = createAsyncThunk<Tasks>("tasksAction/edit-task", async (_, { rejectWithValue }) => {
-// 	try {
-// 		const response = await fetch("http://localhost:5174/edit-task", {
-// 			method: "POST",
-// 			headers: {
-// 				"Content-type": "application/json",
-// 			},
-// 			credentials: "include",
-// 		});
-// 		if (!response.ok) {
-// 			const errorText = await response.text();
-// 			throw new Error(`Error ${response.status}: ${errorText}`);
-// 		}
-// 		const data = await response.json();
-// 		return data;
-// 	} catch (error) {
-// 		return rejectWithValue("Error during marking task as edit!");
-// 	}
-// });
+export const DoneTaskAction = createAsyncThunk<{ ID: number; email: string }, { ID: number; email: string }>(
+	"tasksAction/done-task",
+	async (userData: { ID: number; email: string }, { rejectWithValue }) => {
+		try {
+			const response = await fetch("http://localhost:5174/done-task", {
+				method: "POST",
+				headers: {
+					"Content-type": "application/json",
+				},
+				body: JSON.stringify(userData),
+				credentials: "include",
+			});
+			if (!response.ok) {
+				const errorText = await response.text();
+				throw new Error(`Error ${response.status}: ${errorText}`);
+			}
+			const data = await response.json();
+			return data;
+		} catch (error) {
+			return rejectWithValue("Error during marking task as done!");
+		}
+	}
+);
 
 export const RemoveTaskAction = createAsyncThunk<{ ID: number; email: string }, { ID: number; email: string }>(
 	"tasksAction/remove-task",
@@ -94,8 +78,20 @@ const tasksActionSlice = createSlice({
 				state.loading = false;
 			})
 
+			.addCase(DoneTaskAction.fulfilled, (state, action: PayloadAction<{ ID: number }>) => {
+				state.tasks = state.tasks.filter(task => task.ID !== action.payload.ID);
+				state.loading = false;
+			})
+			.addCase(DoneTaskAction.fulfilled, state => {
+				state.loading = false;
+			})
+
 			.addCase(RemoveTaskAction.fulfilled, (state, action: PayloadAction<{ ID: number }>) => {
 				state.tasks = state.tasks.filter(task => task.ID !== action.payload.ID);
+				state.loading = false;
+			})
+			.addCase(RemoveTaskAction.fulfilled, state => {
+				state.loading = false;
 			});
 	},
 });
