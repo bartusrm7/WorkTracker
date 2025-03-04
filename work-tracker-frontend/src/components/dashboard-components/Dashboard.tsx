c6abad29e8f490cd70283b23f33b03f12ae76b60
Bartek <bbogucki7@gmail.com>
2025-03-04 19:02:16 +0100

    add the ability having views at the amounts of tasks and tasks done at the dashboard

--- work-tracker-frontend/src/components/dashboard-components/Dashboard.tsx
+++ work-tracker-frontend/src/components/dashboard-components/Dashboard.tsx
@@ -13,12 +13,13 @@ export default function Dashboard() {
 	const firstName = useSelector((state: RootState) => state.auth.firstName);
 	const lastName = useSelector((state: RootState) => state.auth.lastName);
 	const tasksData = useSelector((state: RootState) => state.tasks.tasks);
-
 	const [selectedDate, setSelectedDate] = useState(dayjs());
 
 	const filteredTasksData = selectedDate
 		? tasksData.filter(task => dayjs(task.taskDate).isSame(selectedDate, "day"))
 		: tasksData;
+
+	const tasksAmount = filteredTasksData.filter(task => task.taskStatus === "done");
 
 	useEffect(() => {
 		dispatch(UserNamesGetFromBackend());
@@ -54,7 +55,9 @@ export default function Dashboard() {
 			<div className='big-container'>
 				<div className='dashboard__main-container big-separate-container p-2 mb-2 d-lg-flex flex-column justify-content-between'>
 					<div className='dashboard__tracking-container'>
-						<div className='dashboard__tracking-label label'>My tracking</div>
+						<div className='dashboard__tracking-label label'>
+							My tracking <span>{filteredTasksData.length !== 0 ? `(${filteredTasksData.length})` : ""}</span>
+						</div>
 						{filteredTasksData.map((task, index) => (
 							<div className='dashboard__tracking-wrapper' key={index}>
 								<div className='dashboard__task-item'>{task.taskName}</div>
@@ -65,9 +68,13 @@ export default function Dashboard() {
 				<div className='dashboard__main-container big-separate-container p-2 mb-2 d-lg-flex flex-column justify-content-between'>
 					<div className='dashboard__task-container'>
 						<div className='dashboard__task-label label'>
-							Last tasks done <span>{"(ILOŚĆ TASKSÓW)"}</span>
+							Last tasks done <span>{tasksAmount.length !== 0 ? `(${tasksAmount.length})` : ""}</span>
 						</div>
-						<div className='dashboard__task-wrapper'></div>
+						{filteredTasksData.map((task, index) => (
+							<div className='dashboard__task-wrapper' key={index}>
+								<div className='dashboard__task-done-item'>{task.taskStatus === "done" ? task.taskName : ""}</div>
+							</div>
+						))}
 					</div>
 				</div>
 				<div className='dashboard__main-container big-separate-container p-2 mb-2 d-block'>
