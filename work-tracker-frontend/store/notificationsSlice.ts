d07069eb58df27992c04a1dadbe4be07be64e8cc
Bartek <bbogucki7@gmail.com>
2025-03-28 12:06:37 +0100

    create notificationsSlice, implement logic for get acccess notifications and create notifications

---
+++ work-tracker-frontend/store/notificationsSlice.ts
@@ -0,0 +1,88 @@
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface NotificationState {
+	notificationAccess: boolean;
+	notificationName: string;
+	loading: boolean;
+}
+
+const initialState: NotificationState = {
+	notificationAccess: false,
+	notificationName: "",
+	loading: false,
+};
+
+export const AccessForGettingNotifications = createAsyncThunk<
+	{ notificationAccess: boolean },
+	{ notificationAccess: boolean }
+>("notifications/access-notifications", async (_, { rejectWithValue }) => {
+	try {
+		const response = await fetch("http://localhost:5174/access-notifications", {
+			method: "POST",
+			headers: {
+				"Content-type": "application/json",
+			},
+		});
+		if (!response.ok) {
+			const errorText = await response.text();
+			throw new Error(`Error ${response.status}: ${errorText}`);
+		}
+		const data = await response.json();
+		return data;
+	} catch (error) {
+		return rejectWithValue("Error during getting access for notifications!");
+	}
+});
+
+export const CreateNotification = createAsyncThunk<{ notificationName: string }, { notificationName: string }>(
+	"notifications/create-notifications",
+	async (_, { rejectWithValue }) => {
+		try {
+			const response = await fetch("http://localhost:5174/create-notification", {
+				method: "POST",
+				headers: {
+					"Content-type": "application/json",
+				},
+			});
+			if (!response.ok) {
+				const errorText = await response.text();
+				throw new Error(`Error ${response.status}: ${errorText}`);
+			}
+			const data = await response.json();
+			return data;
+		} catch (error) {
+			return rejectWithValue("Error during creating notification!");
+		}
+	}
+);
+
+const notificationSlice = createSlice({
+	name: "notifications",
+	initialState,
+	reducers: {},
+	extraReducers: builder => {
+		builder
+			.addCase(
+				AccessForGettingNotifications.fulfilled,
+				(state, action: PayloadAction<{ notificationAccess: boolean }>) => {
+					state.notificationAccess = action.payload.notificationAccess;
+					state.loading = false;
+				}
+			)
+			.addCase(AccessForGettingNotifications.rejected, (state, action) => {
+				state.loading = false;
+				console.error(action.payload);
+			})
+
+			.addCase(CreateNotification.fulfilled, (state, action: PayloadAction<{ notificationName: string }>) => {
+				state.notificationName = action.payload.notificationName;
+				state.loading = false;
+			})
+			.addCase(CreateNotification.rejected, (state, action) => {
+				state.loading = false;
+				console.error(action.payload);
+			});
+	},
+});
+
+export default notificationSlice.reducer;
--- work-tracker-frontend/store/store.ts
+++ work-tracker-frontend/store/store.ts
@@ -4,6 +4,7 @@ import authSlice from "./authSlice";
 import tasksSlice from "./tasksSlice";
 import tasksActionsSlice from "./tasksActionsSlice";
 import motivationSlice from "./motivationSlice";
+import notificationsSlice from "./notificationsSlice";
 
 const store = configureStore({
 	reducer: {
@@ -12,6 +13,7 @@ const store = configureStore({
 		tasks: tasksSlice,
 		tasksAction: tasksActionsSlice,
 		motivation: motivationSlice,
+		notification: notificationsSlice,
 	},
 });
 
